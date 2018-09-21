94186fa610f482d9ce3c431daf53136adf1e94dd
deepakgouda <deepakgouda1729@gmail.com>
2018-09-21 14:26:24 +0000

    Added window method

---
+++ StockKaggleWindow.py
@@ -0,0 +1,84 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from pandas import read_csv
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+
+def create_dataset(dataset, look_back = 1):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-look_back-1):
+		a = dataset[i:(i+look_back), 0]
+		dataX.append(a)
+		dataY.append(dataset[i + look_back, 0])
+	return np.array(dataX), np.array(dataY)
+
+# np.random.seed(7)
+# dataframe = read_csv('log_inf_small.csv', usecols=[11], 
+# 			engine = 'python', skipfooter=3)
+dataframe = read_csv('sp500.csv')
+# fields = ['timestamp', 'open', 'high', 'low', 'close']
+# dataframe = read_csv('log_inf_small.csv', header = 0, 
+# 	parse_dates = [0], date_parser = parser, index_col = 0, 
+# 	skipinitialspace = True, squeeze = True, usecols = fields)
+
+dataset = dataframe.values
+dataset = dataset.astype('float32')
+
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+
+split = 0.80
+train_size = int(len(dataset)*split)
+test_size = len(dataset)-train_size
+train = dataset[0:train_size,:]
+test = dataset[train_size:len(dataset),:]
+
+look_back = 3
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+
+trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+
+units = 150
+drop = 0.2
+epoch = 10
+
+model = Sequential()
+model.add(LSTM(units, input_shape=(1, look_back)))
+model.add(Dropout(drop))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(trainX, trainY, epochs=epoch, batch_size=1, verbose=1)
+
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+
+trainPredict = scaler.inverse_transform(trainPredict)
+trainY = scaler.inverse_transform([trainY])
+testPredict = scaler.inverse_transform(testPredict)
+testY = scaler.inverse_transform([testY])
+
+trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+print('Train Score: %.2f RMSE' % (trainScore))
+testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+print('Test Score: %.2f RMSE' % (testScore))
+
+trainPredictPlot = np.empty_like(dataset)
+trainPredictPlot[:, :] = np.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+
+testPredictPlot = np.empty_like(dataset)
+testPredictPlot[:, :] = np.nan
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+
+plt.plot(scaler.inverse_transform(dataset))
+plt.plot(trainPredictPlot)
+plt.plot(testPredictPlot)
+plt.title('Units = %d Dropout = %.2f Epoch = %d Split = %d%% Train = %.2f Test = %.2f' % 
+		(units, drop, epoch, split*100, trainScore, testScore))
+plt.show()
